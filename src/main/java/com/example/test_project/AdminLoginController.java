a2aec00de7e1b5b1a044d59ff6ed1432e8c8b815
Md. Abdullah Al Imran Bhuiyan <md.a.a.imran453@gmail.com>
2024-07-01 23:29:27 +0600

    First commit

---
+++ src/main/java/com/example/test_project/AdminLoginController.java
@@ -0,0 +1,4 @@
+package com.example.test_project;
+
+public class AdminLoginController {
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.test_project {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.test_project to javafx.fxml;
+    exports com.example.test_project;
+}
