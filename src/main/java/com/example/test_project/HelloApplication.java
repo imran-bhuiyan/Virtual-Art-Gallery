3030dae33f378364f894bcb0b5df42a554b30ee3
Md. Abdullah Al Imran Bhuiyan <md.a.a.imran453@gmail.com>
2024-07-04 21:08:25 +0600

    1st update 2nd phase

--- src/main/java/com/example/test_project/HelloApplication.java
+++ src/main/java/com/example/test_project/HelloApplication.java
@@ -10,11 +10,13 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("userOrGuestHomePage.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("AdminManageArtwork.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
+
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/test_project/HelloController.java
+++ src/main/java/com/example/test_project/HelloController.java
@@ -1,14 +1,32 @@
 package com.example.test_project;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private Button Chats;
+
+    @FXML
+    private Button Events;
+
+    @FXML
+    private Button Reports;
+
+    @FXML
+    private Button Services;
+
+    @FXML
+    private Button artists;
+
+    @FXML
+    private Button artworks;
+
+    @FXML
+    private Button customers;
+
+    @FXML
+    private Button dashboard;
+
 }
